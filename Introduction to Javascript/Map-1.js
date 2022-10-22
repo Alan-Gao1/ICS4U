bd3430b674d876e287c3ff7c391028ff2e043295
Alan Gao <agao1@bayviewglen.ca>
2022-10-22 18:30:56 -0400

    done map 1 :)

---
+++ Introduction to Javascript/Map-1.js
@@ -0,0 +1,26 @@
+// mapBully
+// const map = {a: "valueA", b: "valueB"};
+// map.b = map.a;
+// map.a = "";
+// console.log(map);
+
+// topping1
+// const map = {"bread": "valueA", "ice-cream": "valueB"};
+// if(map["bread"] != "butter")
+//     map["bread"] = "butter";
+// if(map["ice-cream"] != "cherries")
+//     map["ice-cream"] = "cherries"
+// console.log(map)
+
+// mapAB2
+// const map = {"a": "aaa", "b": "aaa"};
+// const map1 = {"a": "aaa", "b": "bbb", "c": "ccc"};
+// if(map["a"] === map["b"] || map1["a"] === map1["b"])
+//     delete map["a"];
+//     delete map["b"];
+//     delete map1["a"];
+//     delete map1["b"];
+// console.log(map);
+// console.log(map1);
+
+// no more questions because I want to do something more challenging :/
