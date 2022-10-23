52f62d696fe8e79f5c8cabd8a89d7842678bc5fd
Alan Gao <agao1@bayviewglen.ca>
2022-10-23 17:34:56 -0400

    done Map-2 & functions01

---
+++ Introduction to Javascript/Map-2.js
@@ -0,0 +1,34 @@
+// word0
+// let word0 = ["a", "b", "c", "d"]
+// let dict = {};
+// for (let i = 0; i < word0.length; i++) {
+//     dict[word0[i]] = 0;
+// }
+// console.log(dict);
+
+// allSwap
+// let list = ["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"];
+// let temp = "";
+// let used = [];
+// let flag = true;
+// for (let i = 0; i < list.length; i++){
+//     for(let k = 0; k < used.length; k++){
+//         if(i === used[k]){
+//             flag = false;
+//         }
+//     }
+//     for(let j = i+1; j < list.length && flag; j++){
+//         if(list[i].substring(0,1) === list[j].substring(0,1)){
+//             used.push(i);
+//             used.push(j);
+//             temp = list[j];
+//             list[j] = list[i];
+//             list[i] = temp;
+//             flag = false;
+//         }
+//     }
+//     flag = true;
+// }
+// console.log(list)
+
+// done because I'm doing university applications
